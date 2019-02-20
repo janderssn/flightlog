9a4f2e6eec790cd89f9613a66493e3f71b0b12bd
sindre <sindrehauglum@gmail.com>
2019-02-20 15:52:14 +0100

    minor

---
+++ src/main/java/no/hauglum/flightlog/domain/scraper/ScrapeJob.java
@@ -0,0 +1,13 @@
+package no.hauglum.flightlog.domain.scraper;
+
+import no.hauglum.flightlog.domain.BaseEntity;
+
+import javax.persistence.Entity;
+
+/**
+ * For saving data about scrape jobs
+ */
+@Entity
+public class ScrapeJob extends BaseEntity {
+    //TODO
+}
